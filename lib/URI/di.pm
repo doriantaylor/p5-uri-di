de4b7b2761c561272886ffcdcd29ca332dd5cc44
Dorian Taylor <dorian.taylor.lists@gmail.com>
2012-06-12 11:45:08 -0700

    integrating Module::Starter stuff

--- lib/URI/di.pm
+++ lib/URI/di.pm
@@ -251,6 +251,67 @@ sub as_string {
     ${$_[0]};
 }
 
-1;
+=head1 AUTHOR
 
-__END__
+Dorian Taylor, C<< <dorian at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-uri-di at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=URI-di>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc URI::di
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=URI-di>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/URI-di>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/URI-di>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/URI-di/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2012 Dorian Taylor.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    L<http://www.apache.org/licenses/LICENSE-2.0>
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+
+=cut
+
+1; # End of URI::di
